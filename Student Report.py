3d08905491afdf541071524d2f11aaf9ffcb24ce
recklessrashdan <143264915+recklessrashdan@users.noreply.github.com>
2026-06-27 09:01:58 +0530

    Fix input prompts and rename multiply to average
    Correct typos and formatting in the input prompts (use 'your' and add colons). Rename multiply() to average() and update its internal variable and the print call to reflect that it computes the average of the three marks. No functional changes besides clearer names and prompts.

--- Student Report.py
+++ Student Report.py
@@ -1,20 +1,20 @@
 import time
-marks1=int(input("Enter yout 1st mark"))
-marks2=int(input("Enter yout 2nd mark"))
-marks3=int(input("Enter yout 3rd mark"))
+marks1=int(input("Enter your 1st mark: "))
+marks2=int(input("Enter your 2nd mark: "))
+marks3=int(input("Enter your 3rd mark: "))
 
 def total():
     total2=(marks1+marks2+marks3)
     return(total2)
 
-def multiply():
-    multiply2=(marks1+marks2+marks3)/3
-    return(multiply2)
+def average():
+    average2=(marks1+marks2+marks3)/3
+    return(average2)
 
 time.sleep(2)
 
 print("Student Mark Report")
 print("Total Marks-",total())
-print("Average Marks-",multiply())
+print("Average Marks-",average())
 
 time.sleep(5)
